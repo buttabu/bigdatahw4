d5e6290e8fef4d78e1a355578270c0ebebd65944
Abu Butt <buttabu@yahoo.com>
2018-04-26 20:08:55 -0400

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,23 +1,21 @@
 from pyspark import SparkContext
 
 if __name__ == '__main__':
-    sc = SparkContext()
+	sc = SparkContext()
 
-    rest = sc.textFile('/data/share/bdm/nyc_restaurants.csv', use_unicode=False).cache()
-    # rest = sc.textFile('nyc_restaurants.csv', use_unicode=False).cache()
+	rest = sc.textFile('/data/share/bdm/nyc_restaurants.csv', use_unicode=False).cache()
+	
 
-    def extract(partId, lines):
-    if partId == 0:
-        next(lines)
-    import csv
-    for row in csv.reader(lines):
-        yield (row[0], (row[7], 1))
 
-rdd.mapPartitionsWithIndex(extract) \
-    .reduceByKey(lambda x, y: x) \
-    .values() \
-    .reduceByKey(lambda x, y: x + y) \
-    .sortBy(lambda x: -x[1]) \
-    .take(25)
+	def get_count(partId, records):
+	    if partId==0:
+	        records.next()
+	        import csv
+	        reader = csv.reader(records)
+	        for row in reader:
+	            (description)=(row[7])
+	            yield (description,1)
+	restaurants = rest.mapPartitionsWithIndex(get_count)
+	restaurants.take(5)
 
-count = restaurants.reduceByKey(lambda x, y: x + y).saveAsTextFile("output")
+	count = restaurants.reduceByKey(lambda x,y: x+y).saveAsTextFile("output1")
